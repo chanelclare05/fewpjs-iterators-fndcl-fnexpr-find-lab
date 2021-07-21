5a0d37aefb2ca25bd0fcc6e7e209073caa92cd8f
Chanel Donahoe <cdonahoe05@gmail.com>
2021-07-22 09:35:00 +1000

    completed git push

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function superbowlWin(arr){
+   if (arr.find(({result}) => result === "W")) {
+       return (arr.find(({result}) => result === "W").year)
+   } else {
+       return undefined}
+   }
 
+
+// function superbowlWin(arr){
+//     return (arr.find(({result}) => result === "W")).year
+//  }
+ 
+ 
+ 
